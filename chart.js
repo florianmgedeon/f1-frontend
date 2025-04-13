d740a1d7cfad78d37c7cc1692b822e221fc4d665
fged03 <florian.gedeon@gmail.com>
2025-04-13 22:08:24 +0200

    speed gauge added

--- chart.js
+++ chart.js
@@ -1,34 +1,32 @@
 export const dataBuffer = {
-    speed: [],
-    throttle: [],
-    brake: [],
-    gear: [],
-  };
-  
-  export const maxPoints = 20;
-  
-  export const createChart = (ctx, label) =>
-    new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: Array(maxPoints).fill(""),
-        datasets: [{
-          label,
-          data: [],
-          borderColor: "#00ff99",
-          borderWidth: 2,
-          pointRadius: 0,
-          fill: false,
-        }],
+  throttle: [],
+  brake: [],
+  gear: [],
+};
+
+export const maxPoints = 20;
+
+export const createChart = (ctx, label) =>
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: Array(maxPoints).fill(""),
+      datasets: [{
+        label,
+        data: [],
+        borderColor: "#00ff99",
+        borderWidth: 2,
+        pointRadius: 0,
+        fill: false,
+      }],
+    },
+    options: {
+      responsive: false,
+      animation: false,
+      plugins: { legend: { display: false } },
+      scales: {
+        x: { display: false },
+        y: { display: false },
       },
-      options: {
-        responsive: false,
-        animation: false,
-        plugins: { legend: { display: false } },
-        scales: {
-          x: { display: false },
-          y: { display: false },
-        },
-      }
-    });
-  
+    }
+  });
